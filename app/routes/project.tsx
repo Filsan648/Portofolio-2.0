e45a37bcb44f06f658b9aa88b493b785225c5158
Filsan <filsanfouad2002@gmail.com>
2026-04-27 08:50:25 +0300

    update

--- app/routes/project.tsx
+++ app/routes/project.tsx
@@ -3,14 +3,17 @@ import Footer from "../Generale/Footer";
 import Menue from "~/Generale/Menue";
 import { useParams } from "react-router";
 import ProjectDescription from "~/Compement/work/project_description";
+import { Projects } from "~/data/DataProject";
 export function meta({}: Route.MetaArgs) {
+    const { id } = useParams();
+       const newsId = Number(id!)
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: Projects[newsId].title },
+    { name: "description", content: Projects[newsId].description },
   ];
 }
 
-export default function Projects() {
+export default function Projectes() {
        const { id } = useParams();
        const newsId = Number(id!)
   return <div className=" flex flex-col gap-40 "> <Menue />  <ProjectDescription id={newsId} /> <Footer />   </div>;
